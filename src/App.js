6a664992998362c63410250df0059ce478da286a
sjproctor <sjproctor950@gmail.com>
2021-10-01 10:30:37 -0700

    adds tic tac toe base logic

--- src/App.js
+++ src/App.js
@@ -6,15 +6,84 @@ class App extends Component{
   constructor(props){
     super(props)
     this.state = {
-      squares: [0, 0, 0, 0, 0, 0, 0, 0, 0]
+      squares: Array(9).fill(null),
+      currentPlayer: "❌",
+      winner: null
     }
   }
-
+  playerSelection = (currentClick) => {
+    const { squares, currentPlayer, winner } = this.state
+    if(squares[currentClick] === null && !winner){
+      squares[currentClick] = currentPlayer
+      this.setState({
+        squares: squares,
+        currentPlayer: currentPlayer === "❌" ? "⭕️" : "❌"
+      })
+    }
+    this.winning()
+  }
+  winning = () => {
+    const winningConditions = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+    winningConditions.forEach(value => {
+      const [first, second, third] = value
+      const { squares } = this.state
+      if(
+        squares[first] === "❌" &&
+        squares[second] === "❌" &&
+        squares[third] === "❌"
+      ){
+        this.setState({winner: "❌"})
+      } else if(
+        squares[first] === "⭕️" &&
+        squares[second] === "⭕️" &&
+        squares[third] === "⭕️"
+      ){
+        this.setState({winner: "⭕️"})
+      }
+    })
+  }
+  restartGame = () => {
+    this.setState({
+      squares: Array(9).fill(null),
+      currentPlayer: "❌",
+      winner: null
+    })
+  }
   render(){
+    const catsGame = this.state.squares.every(value => value !== null)
     return(
       <>
         <h1>Tic Tac Toe</h1>
-        <Square />
+        {this.state.winner && <h3>{this.state.winner} wins!</h3>}
+        {(catsGame && !this.state.winner) && <h3>Cats Game</h3>}
+        {(this.state.winner || catsGame)&&
+          <div className="button">
+            <button onClick={this.restartGame}>
+              Play Again
+            </button>
+          </div>
+        }
+        <div className="gameboard">
+          {this.state.squares.map((value, index) => {
+            return(
+              <Square
+                value={value}
+                index={index}
+                key={index}
+                playerSelection={this.playerSelection}
+              />
+            )
+          })}
+        </div>
       </>
     )
   }
